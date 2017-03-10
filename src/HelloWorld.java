5fb66c0fcaf8a797d390739efcdf96d709133976
NIKHIL REMALLI <remallinikhil7@gmail.com>
2017-03-10 17:36:10 -0500

    Java
    Edited one

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
+// Nikhil Remalli
+public class HelloWorld {// class starts here
 
-public class HelloWorld {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) {// main starts here
 		System.out.println("Hello WOrld");
 
-	}
+	}// main ends here
 
-}
+}// class ends here
